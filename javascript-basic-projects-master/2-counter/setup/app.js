8289a662fd7f2fc486afdcb0a368561afbbdc220
shawnbenn <shawnbennprojects@gmail.com>
2020-08-03 15:47:57 -0400

    first commit

---
+++ javascript-basic-projects-master/2-counter/setup/app.js
@@ -0,0 +1,27 @@
+// set intitial count
+let count = 0;
+
+
+// select value adn buttons
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            count--;
+        } else if (styles.contains('increase')) {
+            count++;
+        } else {
+            count = 0
+        }
+
+        count > 0 ? value.style.color = 'green' : value.style.color = 'red';
+        if (count === 0) {
+            value.style.color = 'black'
+        }
+        value.textContent = count;
+    })
+})
